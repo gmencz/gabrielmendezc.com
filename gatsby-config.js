87a534572a86fef7df1da2f891b30b447f7e7c84
Gabriel Mendez <gabrielmendez.dev@gmail.com>
2020-03-07 00:30:05 +0100

    fixed gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,13 +59,6 @@ module.exports = {
       }
     },
     {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'fonts',
-        path: `${__dirname}/src/fonts`
-      }
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'Gabriel Méndez',
