598a655340f7120c138115e71f5e6e1834648a42
Gabriel Mendez <gabrielmendez.dev@gmail.com>
2020-03-10 22:12:04 +0100

    change google analytics to head

--- gatsby-config.js
+++ gatsby-config.js
@@ -96,7 +96,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'none'
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'none',
+        head: true
       }
     },
     `gatsby-plugin-netlify-cache`,
