56e2993321ecb5084750421054840a41782a55de
Gabriel Méndez <gabrielmendez.dev@gmail.com>
2020-09-08 20:10:48 +0200

    :package: Install emotion-theming

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,9 @@ const Home: React.FC = () => {
     <Layout>
       <h1>Hi! I&apos;m Gabriel.</h1>
       <p>
-        I&apos;m a developer living in Vigo, Spain. You&apos;ve found my
-        personal corner of the internet where I like to spread knowledge and
-        share my opinion on random topics from time to time.
+        I&apos;m a developer living in Vigo, Spain. This is my personal corner
+        of the internet where I like to spread knowledge and share my opinion on
+        random topics from time to time.
       </p>
     </Layout>
   )
